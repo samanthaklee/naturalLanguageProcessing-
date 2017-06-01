8bf37af30f1a133d19f9ae3409a38dc43d3c2876
Xiaoxiong Xu <xiaoxiong0601@gmail.com>
2017-05-31 20:35:02 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 import io
 import preprocessing
 import pandas as pd
+import csv
 
 def status_processing(corpus):
     #myCorpus = preprocessing.NLTKPreprocessor(corpus)
@@ -21,6 +22,9 @@ def status_processing(corpus):
     print ("Lemmatization...")
     myCorpus.lemmatization()
 
+    print ("Pos tagging")
+    myCorpus.pos_tag()
+
     return (myCorpus.stopwords())
     #print ("Correcting the words...")
     #myCorpus.spell_correct()
@@ -31,7 +35,6 @@ def status_processing(corpus):
     #word_final = myCorpus.untokenizing()
 
     #return word_final
-
 
 if __name__ == '__main__':
     #dtype_dic = {'Summary': str}
@@ -45,4 +48,13 @@ if __name__ == '__main__':
     #print (txt_corpus.dtypes)
     word_final = status_processing(summary)
     print ("End of the Pre-Processing Process ")
-    print (word_final)
+    print(word_final)
+
+with open('output.csv','w') as csvfile:
+        fieldnames = ['Words','Accuracy']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+
+        i = 0
+        for i in range(len(word_final)):
+            writer.writerow({'Words': word_final[i][0], 'Accuracy': word_final[i][1]})
